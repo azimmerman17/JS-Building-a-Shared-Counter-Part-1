d3d4fbe6848201c5346df57b080c822ce36a3061
Andrew Zimmerman <azimmerman@pga.com>
2022-10-23 13:25:20 -0400

    Used fetch to get value

--- public/index.js
+++ public/index.js
@@ -1,9 +1,12 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+    let response = await fetch('http://127.0.0.1:9001/counter')
+    let responseObj = await response.json()
+    console.log(responseObj)
+    let countValue = responseObj.value;
 
     function increment(){
         countValue++;
